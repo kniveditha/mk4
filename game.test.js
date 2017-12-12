483e889fb6b5ccdbc7bbb1618dda1233592520a5
kniveditha <kniveditha777@gmail.com>
2017-12-12 12:14:53 +0530

    adding circleci config

--- game.test.js
+++ game.test.js
@@ -9,12 +9,12 @@ Warrior = require("./game").Warrior;
 attack = require("./game").attack;
 getWarriors = require("./game").getWarriors;
 
-// describe("Test Calculate bonus", () => {
-//   it("should return a bonus of 6", () => {
-//     expect(scorpian.armorBonus).to.equal(0);
-//     //fix me --> armorBonus is always returning zero
-//   });
-// });
+describe("Test Calculate bonus", () => {
+  it("should return a bonus of 6", () => {
+    expect(scorpian.armorBonus).to.equal(6);
+    //fix me --> armorBonus is always returning zero
+  });
+});
 
 // describe("Test Roll Dice", () => {
 //   it("should never return a value of zero", () => {
@@ -26,44 +26,44 @@ getWarriors = require("./game").getWarriors;
 //   });
 // });
 
-// describe("Test", () => {
+describe("Test", () => {
   
-//     var LiuKang;
-//     beforeEach(function(done) {
-//         var leatherArmor = new Armor("Leather Armor", 3);
-//         var sword = new Weapon("Chang Sword", 6);
-//         LiuKang = new Warrior("Lui Kang", 6, 8, [leatherArmor, sword]);
-//         setTimeout(done, 300);
-//     });
+    var LiuKang;
+    beforeEach(function(done) {
+        var leatherArmor = new Armor("Leather Armor", 3);
+        var sword = new Weapon("Chang Sword", 6);
+        LiuKang = new Warrior("Lui Kang", 6, 8, [leatherArmor, sword]);
+        setTimeout(done, 300);
+    });
 
-//     it('response should be null', function() {
-//       expect(LiuKang.strength).to.equal(6);
-//     });
+    it('response should be null', function() {
+      expect(LiuKang.strength).to.equal(6);
+    });
 
-//     afterEach(function(done) {
-//        LiuKang = undefined;
-//        setTimeout(done, 300);
-//     });
+    afterEach(function(done) {
+       LiuKang = undefined;
+       setTimeout(done, 300);
+    });
    
-// });
+});
 
-// describe("Test", () => {
-// var enemyArmor, enemySkill, warriorStrength, rollDiceFunc;
-//   beforeEach(function(done) {
-//          enemyArmor = 5;
-//          enemySkill = 6;
-//          warriorStrength= 6;
-//         rollDiceFunc = (val1, val2) => {
-//           return 20;
-//         }
-//         setTimeout(done, 300);
-//     });
+describe("Test", () => {
+var enemyArmor, enemySkill, warriorStrength, rollDiceFunc;
+  beforeEach(function(done) {
+         enemyArmor = 5;
+         enemySkill = 6;
+         warriorStrength= 6;
+        rollDiceFunc = (val1, val2) => {
+          return 20;
+        }
+        setTimeout(done, 300);
+    });
   
-//     it('should return true when calling attack', function() {
-//       expect(attack(enemyArmor, enemySkill, warriorStrength, rollDiceFunc)).to.equal(true);
-//     });
+    it('should return true when calling attack', function() {
+      expect(attack(enemyArmor, enemySkill, warriorStrength, rollDiceFunc)).to.equal(true);
+    });
    
-// });
+});
 
 describe("test getWarriors", () => {
  // var allWarriors = getWarriors(err,data);
